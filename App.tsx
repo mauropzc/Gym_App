fe30c062731c3a4b6af928f92bd34d6579e9e7b2
mauropzc <114313585+mauropzc@users.noreply.github.com>
2024-03-16 21:28:05 -0500

    tercer commit
    Se cambio el texto principal

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola a todos!</Text>
+      <Text>Hola que tal</Text>
       <StatusBar style="auto" />
     </View>
   );
